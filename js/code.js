06100ff741acf38094c07dc987b50573c2c5234f
MiguelLopez2020 <miguel_angel_lopez_s@hotmail.com>
2021-04-20 15:25:38 -0500

    ajustes de diseño

--- js/code.js
+++ js/code.js
@@ -9,9 +9,9 @@ function primo() {
     var a = document.getElementById("num2").value
 
     if (a % 2) {
-        document.getElementById("pr").innerHTML = ("no es par")
+        document.getElementById("pr").innerHTML = (a + " es impar")
     } else {
-        document.getElementById("pr").innerHTML = (" es par")
+        document.getElementById("pr").innerHTML = (a + " es par")
     }
 }
 
@@ -19,17 +19,17 @@ function pizeria() {
     var a = document.getElementById("age").value
 
     if (a <= 10) {
-        document.getElementById("premio").innerHTML = ("tu premio es un jugo")
+        document.getElementById("premio").innerHTML = ("Felicitaciones tu premio es un jugo")
     } else if (a >= 18) {
-        document.getElementById("premio").innerHTML = ("tu premio es una cerveza")
-    } else document.getElementById("premio").innerHTML = ("no recibes ningun premio")
+        document.getElementById("premio").innerHTML = ("Felicitaciones tu premio es una cerveza")
+    } else document.getElementById("premio").innerHTML = ("no recibes ningun premio por el momento")
 
     var b = document.getElementById("gen").value
 
     if (a != " " && b === "mujer") {
-        document.getElementById("premio").innerHTML = ("tu premio es una pizza hawaiana")
+        document.getElementById("premio").innerHTML = ("Felicitaciones tu premio es una pizza hawaiana")
     } else if (a != " " && b === "hombre") {
-        document.getElementById("premio").innerHTML = ("tu premio es una pizza 3 carnes")
+        document.getElementById("premio").innerHTML = ("Felicitacions tu premio es una pizza 3 carnes")
     }
 }
 
@@ -59,18 +59,18 @@ function ValorAPagar() {
 function Resultado() {
     var x = document.getElementById("numero").value
     for (var a = 1; a <= 10; a++)
-        document.getElementById("tdm").innerHTML = (x + a + (x * a)+"<br>");        
+        document.getElementById("tdm").innerHTML = (x + "X" + a + "=" + (x * a) + "<br>");
 }
 
 function informe() {
     var prom = document.getElementById("promedio").value
     var matricula = 1000000
-    var nota = "Su descuento es del "
+    var nota = "Su descuento es de "
     if (prom <= 3) {
-        console.log("no se hace descuento")
+        document.getElementById("dto").innerHTML=("no se hace descuento")
     } else if (prom > 3 && prom <= 4) {
-        console.log(nota + parseInt(((matricula * 5) / 100) - matricula))
+        document.getElementById("dto").innerHTML=(nota + parseInt(((matricula * 95) / 100) - matricula))
     } else if (prom > 4) {
-        console.log(nota + parseInt(((matricula * 50) / 100) - matricula))
+        document.getElementById("dto").innerHTML=(nota + parseInt(((matricula * 50) / 100) - matricula))
     }
 }
